54787382c5a96681cc9b68dd2f99003ce68def0a
Richard Munoz <rmunoz@cs.columbia.edu>
2015-12-05 12:46:07 -0500

    paste inherited attributes into each group list

--- cast.ml
+++ cast.ml
@@ -1,13 +1,24 @@
 open Sast
 
-let check_setup env setup =
-  (* Not implemented *)
-  [], [], []
+let rec get_attributes g = match g.extends with
+    None -> g.attributes
+  | Some(par) ->
+      let pa = get_attributes par in
+      pa @ g.attributes
 
-let check_turns env turns =
-  []
+let rec order_attrib = function
+    [] -> []
+  | g :: rest ->
+      let a = get_attributes g in
+      let new_g = {g with attributes = a} in
+      new_g :: order_attrib rest
 
-let group_eval program =
+let group_eval (program : Sast.program) =
+  let setup, turns = program in
+  let v, f, g = setup in
+  let g = order_attrib g in
+  let setup = v, f, g in
+  let program = setup, turns in
   program
 
 let print_program (program : Sast.program) =
